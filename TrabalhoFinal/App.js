220a8262e65172719d82048e200f6fc54b874069
poemabochner <poemabochner8@gmail.com>
2022-11-24 00:18:31 -0300

    adição de splash screen animada

--- TrabalhoFinal/App.js
+++ TrabalhoFinal/App.js
@@ -1,14 +1,31 @@
-import { NavigationContainer } from "@react-navigation/native";
+
+import { NavigationContainer, StackActions } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { RotasPrivadas } from "./src/routes/RotasPrivadas";
-import { StatusBar } from "react-native";
-// import { RotasPublicas } from "./src/routes/RotasPublicas";
-
-// import { Routes } from './src/routes';
-
-const Tab = createBottomTabNavigator();
+import { StatusBar, Image, View } from "react-native";
+import { useEffect, useState } from "react";
 
 export default function App() {
+
+const [load, setLoad] = useState(true)
+function SplashScreenVideo (){
+  setTimeout(() => {
+    setLoad(false);
+  }, 5000);
+    
+}
+
+useEffect(()=>{
+  SplashScreenVideo()
+},[])
+
+if (load){
+  return(
+    <View  style={{flex:1}} >
+    <Image style={{width:"100%", height:"100%"}} source={require('./assets/splash.gif')}/>
+    </View>
+  )
+}
   return (
     <NavigationContainer>
       <StatusBar
@@ -20,4 +37,5 @@ export default function App() {
       <RotasPrivadas />
     </NavigationContainer>
   );
+
 }
